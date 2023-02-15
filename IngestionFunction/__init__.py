788d038588c9a7f6f75609192c7b9432356a8880
alejopelaez24 <pelaezalejandrop@gmail.com>
2023-02-15 15:25:41 -0500

    primer commit

--- IngestionFunction/__init__.py
+++ IngestionFunction/__init__.py
@@ -4,13 +4,13 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Python HTTP trigger functionnnnnnnnnnnn processed a request.')
 
     name = req.params.get('name')
     if not name:
         try:
             req_body = req.get_json()
-        except ValueError:
+        except ValueError:     
             pass
         else:
             name = req_body.get('name')
