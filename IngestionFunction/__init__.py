6f35ed6237ff8f4fbe229d4d9b69124d76ef0472
alejopelaez24 <pelaezalejandrop@gmail.com>
2023-02-17 06:52:19 -0500

    Lectura de API valor acciones según lista

--- IngestionFunction/__init__.py
+++ IngestionFunction/__init__.py
@@ -1,24 +1,29 @@
 import logging
-
+import requests
+import pandas as pd
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger functionnnnnnnnnnnn processed a request.')
+    try:
+        logging.info('Python HTTP trigger function processed a request.')
+        actionList = ["AAPL", "MSFT", "TSLA", "MSTR", "META", "CRM", "AMZN"]
+        print(actionList)
+        for x in actionList:
+            readAPIActions(x)
+        logging.info('Se leyó correctamente la API de acciones con información de AAPL')
+        return func.HttpResponse('Proceso completado con exito', status_code=200)
+    
+    except Exception as e:
+        print(str(e))
+        logging.error('Ocurrio un error en: ' + str(e))
+        return func.HttpResponse('Ocurrio un error en: ' + str(e), status_code=400)
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:     
-            pass
-        else:
-            name = req_body.get('name')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+def readAPIActions(action):
+    #Apple
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol={action}&interval=60min&outputsize=compact&apikey=4EVM3NBBM0PCESWU'
+    r = requests.get(url, headers={'open': 'close'})
+    df = pd.DataFrame.from_dict(r.json()["Monthly Time Series"],orient='index')
+    appl_df = df.assign(Symbol = action)
+    print(appl_df)
